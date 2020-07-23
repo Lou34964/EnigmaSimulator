c771c1458cd1cfcca46c518026fe94d0c41ef0e3
Chad <Mapletonkid@gmail.com>
2020-07-23 10:42:02 -0600

    Reflectors added

---
+++ EnigmaSimulator/Reflectors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaSimulator
+{
+    public static class Reflectors
+    {
+        public static Reflector I = new Reflector();
+        public static Reflector II = new Reflector();
+        public static Reflector III = new Reflector();
+        public static void SetReflectors()
+        {
+            string FileDataRaw = File.ReadAllText(@"./Reflector1Config.txt");
+            I.SetReflector(FileDataRaw.Split(','));
+            FileDataRaw = File.ReadAllText(@"./Reflector2Config.txt");
+            II.SetReflector(FileDataRaw.Split(','));
+            FileDataRaw = File.ReadAllText(@"./Reflector3Config.txt");
+            III.SetReflector(FileDataRaw.Split(','));
+        }
+    }
+}
